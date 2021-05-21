e16473abe42085e168588ba05635ea80a1013743
EC2 Default User <ec2-user@ip-172-31-27-252.ec2.internal>
2021-05-21 03:39:43 +0000

    Use firebase

---
+++ src/lib/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDG94NLkkQQJohQdIYH4B8-W8gUZZVrnwk",
+  authDomain: "nandemoii-a15a3.firebaseapp.com",
+  databaseURL: "https://nandemoii-a15a3-default-rtdb.firebaseio.com",
+  projectId: "nandemoii-a15a3",
+  storageBucket: "nandemoii-a15a3.appspot.com",
+  messagingSenderId: "324266812975",
+  appId: "1:324266812975:web:9aac052112a1ef7d0248da"
+};
+
+firebase.initializeApp(firebaseConfig); 
